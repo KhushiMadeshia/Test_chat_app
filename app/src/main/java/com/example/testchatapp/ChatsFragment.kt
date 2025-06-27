6e17849c2331fad2d1e9d661ec7e2c8ddd8ecd7f
KhushiMadeshia <khushimadeshia@gmail.com>
2025-06-28 01:08:30 +0530

    commit

--- app/src/main/java/com/example/testchatapp/ChatsFragment.kt
+++ app/src/main/java/com/example/testchatapp/ChatsFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.content.Intent
+import com.cometchat.chat.constants.CometChatConstants
+import com.cometchat.chat.models.Conversation
+import com.cometchat.chat.models.Group
+import com.cometchat.chat.models.User
+import com.cometchat.chatuikit.conversations.CometChatConversations
+
 
 class ChatsFragment : Fragment() {
 
-
+    private lateinit var conversationsView: CometChatConversations
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +25,32 @@ class ChatsFragment : Fragment() {
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        conversationsView = view.findViewById(R.id.conversation_view)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        conversationsView.setOnItemClick { _, _, conversation ->
+            startMessageActivity(conversation)
+        }
+    }
+
+    private fun startMessageActivity(conversation: Conversation) {
+        val intent = Intent(requireContext(), MessageActivity::class.java).apply {
+            when (conversation.conversationType) {
+                CometChatConstants.CONVERSATION_TYPE_GROUP -> {
+                    val group = conversation.conversationWith as Group
+                    putExtra("guid", group.guid)
+                }
+                else -> {
+                    val user = conversation.conversationWith as User
+                    putExtra("uid", user.uid)
+                }
+            }
+        }
+        startActivity(intent)
+    }
 }
 
